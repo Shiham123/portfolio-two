6db24f19ecf5d02a4fec9a76ad1cd5adac549623
Shiham123 <s36651786@gmail.com>
2023-12-11 17:20:20 +0600

    my work section started

--- src/Section/MyWorkSection/MyWorkSection.jsx
+++ src/Section/MyWorkSection/MyWorkSection.jsx
@@ -1,5 +1,14 @@
+import FullStack from '../../assets/Svg/fullStack.svg';
+
 const MyWorkSection = () => {
-  return <div>this is my work section</div>;
+  return (
+    <div>
+      <div>
+        <img src={FullStack} alt="" />
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default MyWorkSection;
