c164c4360df7473f0026afb16f98bd43c6a9f61d
Shiham123 <s36651786@gmail.com>
2023-12-11 15:07:46 +0600

    navbar complete

--- src/Section/NavbarSection/NavbarSection.jsx
+++ src/Section/NavbarSection/NavbarSection.jsx
@@ -1,5 +1,85 @@
+import { NavLink } from 'react-router-dom';
+import LogoLight from '../../assets/_06cc4017-b38b-4b61-a4a2-2871c9b6d694.jpg';
+
+import { FaBars } from 'react-icons/fa';
+
+const routeData = [
+  { label: 'Home', to: '/' },
+  { label: 'Contact', to: '/contact' },
+  { label: 'Education', to: '/education' },
+  { label: 'Experience', to: '/experience' },
+  { label: 'Project', to: '/project' },
+];
+
 const NavbarSection = () => {
-  return <div>this is navbar section</div>;
+  return (
+    <div>
+      <div className="navbar bg-colorOne px-8 py-4">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="lg:hidden">
+              <FaBars />
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {routeData.map((item, index) => {
+                const { label, to } = item;
+                return (
+                  <div key={index}>
+                    <NavLink to={to}>
+                      <li>{label}</li>
+                    </NavLink>
+                  </div>
+                );
+              })}
+            </ul>
+          </div>
+          <NavLink to={'/'}>
+            <div className="flex justify-center items-center gap-4">
+              <img
+                src={LogoLight}
+                width={50}
+                height={50}
+                className="rounded-full"
+                alt=""
+              />
+              <h1 className="font-caveat text-3xl font-bold">Shiham</h1>
+            </div>
+          </NavLink>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <div className="flex gap-8">
+              {routeData.map((item, index) => {
+                const { label, to } = item;
+                return (
+                  <div key={index}>
+                    <NavLink
+                      to={to}
+                      className={({ isActive }) =>
+                        isActive
+                          ? 'font-playfairDisplay text-xl font-semibold tracking-wider'
+                          : 'font-playfairDisplay text-xl tracking-wider'
+                      }
+                    >
+                      <li className="hover:bg-colorThree px-4 py-2 rounded-lg duration-300 hover:text-colorOne">
+                        {label}
+                      </li>
+                    </NavLink>
+                  </div>
+                );
+              })}
+            </div>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Button</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavbarSection;
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import Routes from './Router';
+import './main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
